b06f1fb974c9f94d5e894b84ed22a16b263e905a
Arni Saha <40070818+arnisaha@users.noreply.github.com>
2022-05-05 19:28:58 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,12 @@
 provider "aws" {
-    profile = "arniaws"
-    region = "us-east-2"
+    profile = "default"
+    region = "us-east-1"
+}
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-example-bucket"
 }
 
-resource "aws_s3_bucket" "b" {
-    bucket = "myawsbucket125463584"
-    acl = "private"
-
-    tags = {
-        Name = "My AWS Bucket"
-        Environment = "Dev"
-    }
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
 }
-
